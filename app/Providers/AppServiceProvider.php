b8c0c2fadce1c7cb000ed0e5775b52009a8ef196
Yurei <69757457+Yurei21@users.noreply.github.com>
2026-03-27 23:24:27 +0800

    Adding OAuth

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (request()->isSecure()) {
+        if (app()->environment('production')) {
             URL::forceScheme('https');
         }
 
